af97733d6a51d4cd62fa3f1afe8680769d6fecb0
Alexandre Martins <alemartins720@gmail.com>
2020-10-29 16:13:34 -0300

    teste de conexao

---
+++ conecta.php
@@ -0,0 +1,14 @@
+<?php
+	// conexão com o banco de dados
+
+	$banco = "flex";
+	$usuario = "alexandre";
+	$senha = "2404";
+	$hostname = "localhost";
+	$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die ("Não foi possível conectar ao banco mysql");
+		if (!$conn) {echo "Não foi possivel conectar ao banco mysql"; exit;}
+		else {echo "parabéns! A conexão ao banco de dados ocorreu normalmente!";}
+
+	mysql_close();
+
+?>
